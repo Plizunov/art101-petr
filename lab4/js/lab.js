d97a16b4e5ef6d73a32867ca5ea50bdcfe3f907c
plizunov <plizunov@ucsc.edu>
2021-01-28 00:17:29 -0800

    complete lab 4 and correct the entry in the directory index

---
+++ lab4/js/lab.js
@@ -0,0 +1,26 @@
+
+//Fist task list
+/*
+* 1. Divide the task into baby steps. Each step should have a single action, not several
+* 2. Start each step with an action word
+* 3. Follow a logical progression
+* 4. Choose your words carefully
+* 5. Use positive actions
+* 6. Write in second person
+* 7. Include alternatives
+* 8. Use graphics where helpful
+*/
+
+
+//Second task list
+/*
+* 1. Start at the beginnign of the lap
+* 2. Race to the finish line
+** 2a. Avoid opponents, if there are any
+** 2b. Complete proper turns
+** 2c. Maintain first place
+* 3. Cross the finish line
+** 3a. Check for validity of players path throughout the race
+** 3b. Check the players position in the race compared to opponents
+* 4. If player finished 3rd, 2nd or 1st, issue congratulations
+*/
